1d04345d741c96941919d4da4b2a2d56349c91ea
mkruszynski <mkruszynski@soldevelo.com>
2017-04-26 15:20:36 +0200

    #26: Parametrization of the base OIM address

--- openinfoman-ilr.sh
+++ openinfoman-ilr.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+: ${ILR_HOST=localhost}
+
+export ILR_HOST
+
 cd ~/
 cd /root/basex/bin && nohup ./basexhttp &
 sudo add-apt-repository ppa:openhie/release
@@ -7,7 +11,7 @@ sudo apt-get update
 
 echo "openinfoman openinfoman/dbname string provider_directory" | debconf-set-selections
 echo "openinfoman openinfoman/memsize string 1g" | debconf-set-selections
-echo "openinfoman openinfoman/baseurl string https://localhost:5000" | debconf-set-selections
+echo "openinfoman openinfoman/baseurl string https://$ILR_HOST:5000" | debconf-set-selections
 
 sudo apt-get -y install openinfoman-ilr
 sudo apt-get install openinfoman-dhis
