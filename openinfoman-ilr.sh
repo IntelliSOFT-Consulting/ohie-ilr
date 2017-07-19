ba4dcd8f4b9708580875e458b6085b927886299c
Marcin Kruszyński <mkruszynski@soldevelo.com>
2017-07-19 13:35:49 +0200

    #112: ILR_HOST variable removed

--- openinfoman-ilr.sh
+++ openinfoman-ilr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-: ${ILR_HOST=localhost}
+: ${SERVER_NAME=localhost}
 
-export ILR_HOST
+export SERVER_NAME
 
 cd ~/
 cd /root/basex/bin && nohup ./basexhttp &
@@ -11,7 +11,7 @@ sudo apt-get update
 
 echo "openinfoman openinfoman/dbname string provider_directory" | debconf-set-selections
 echo "openinfoman openinfoman/memsize string 1g" | debconf-set-selections
-echo "openinfoman openinfoman/baseurl string https://$ILR_HOST:5000" | debconf-set-selections
+echo "openinfoman openinfoman/baseurl string https://$SERVER_NAME:5000" | debconf-set-selections
 
 sudo apt-get -y install openinfoman-ilr
 sudo apt-get install openinfoman-dhis
